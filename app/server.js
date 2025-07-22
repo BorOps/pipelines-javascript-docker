e4cd7044320668d6ce1f20dadcd9c5d495cab7bf
BorOps <138872160+BorOps@users.noreply.github.com>
2025-07-22 15:15:06 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Boro Rondo!')
+  res.send('Hello World from Boro Rondo!!')
 })
 
 app.listen(port, () => {
